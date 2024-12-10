516198eb080a460a82162dd3d712a548cc8eb63e
johntwothreefour <john.espino.4@gmail.com>
2024-12-10 01:24:39 -0800

    Update riddle.js
    edit grammar

--- assets/js/riddle.js
+++ assets/js/riddle.js
@@ -1,9 +1,9 @@
 // Define a pool of riddles and their answers
 const riddles = [
-    { question: "I am never ending hunger - My fury, blistering heat. I do not what a touch might feel like, but in darkness I make shadows retreat.", answer: "fire" },
-    { question: "I am inevitable. All things must one day succumb to me, from small birds, to foul beasts. Neither stone nor steel can prolong and resist. Your deaths I will claim without fail, I insist.", answer: "time" },
+    { question: "I am never ending hunger - My fury, blistering heat. I do not know what a touch might feel like, but in darkness I make shadows retreat.", answer: "fire" },
+    { question: "I am inevitable. All things must one day succumb to me, from small birds, to foul beasts. Neither stone nor steel can prolong or resist. Your deaths I will claim without fail, I insist.", answer: "time" },
     { question: "I soar the skies, wingless... I cry without eyes. Whenever, wherever I may go, darkness surely arrives", answer: "cloud" },
-    { question: "Neither friend, nor foe. You will never truly know. Though I am always ahead of you, you never can see me... can you?", answer: "future" }
+    { question: "Neither friend, nor foe. You will never truly know. Though I am always ahead and always true, you never can see me... can you?", answer: "future" }
 ];
 
 // Select a random riddle from the pool
